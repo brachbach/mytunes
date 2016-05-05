6054ed663fa70107d18da4cc7fe63391f6cc1010
brachbach <brachbach@gmail.com>
2016-05-05 11:54:08 -0700

    Implement fetching songs from server

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,17 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  url: 'https://api.parse.com/1/classes/songs/',
+  
+  model: SongModel,
+
+  parse: function(response) {
+    return response.results;
+  },
+
+  initialize: function() {
+    this.fetch();
+    // console.log(this);
+  },
 
 });
