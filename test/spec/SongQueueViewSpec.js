55cd6d6d1be72b1607376962bc1dd88dec426f98
brachbach <brachbach@gmail.com>
2016-05-05 16:25:36 -0700

    Fixed the test

--- test/spec/SongQueueViewSpec.js
+++ test/spec/SongQueueViewSpec.js
@@ -38,9 +38,10 @@ describe('SongQueueView', function() {
   it('removes songs from the queue when they are clicked', function() {
     view = new SongQueueView({collection: fakeSongs});
     view.render();
-    console.log(view.el.children().get(1));
-    view.$el.children().get(1).click();
-    view.$el.children().get(1).click();
+    // console.log($('tr'));
+    // $('tr').click();
+    view.$el.find('tr').first().click();
+    view.$el.find('tr').first().click();
     // console.log(JSON.stringify(view.$el.children().get(1)));
     expect(view.collection.length).to.equal(0);
   });
