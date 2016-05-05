089e0da547e17f70f263ae0c5710a085d16fa6f3
brachbach <brachbach@gmail.com>
2016-05-04 17:30:12 -0700

    Implemented viewing of songQueue

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,18 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add remove', this.render, this);
   },
 
+  //for each song in song queue
+  //  create a songqueueEntryView
+  //  add it to the songqueueview html string
+  //return the html
   render: function() {
-    return this.$el;
+    this.$el.append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
-
 });
